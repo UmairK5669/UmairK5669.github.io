b2236c72cfd0fd0a65013f2dd7a5c658046a8556
Sameer Khan <sa74khan@edu.uwaterloo.ca>
2020-09-16 02:32:06 -0400

    Added products in cart to checkotu page

--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,11 @@ import "./Header.css"
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 import { Link } from "react-router-dom"
+import { useStateValue } from './StateProvider';
 
 function Header() {
+    const [{ basket }, dispatch] = useStateValue();
+
     return (
         <div className="header">
             <Link to="/">
@@ -49,8 +52,8 @@ function Header() {
 
                         <ShoppingBasketIcon />
                         <span className="header_option2 header_basketCount">
-                            0
-                    </span>
+                            {basket.length}
+                        </span>
                     </div>
                 </Link>
 
