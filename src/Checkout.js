aa34e89750ec99105b81bebf4ebb9f5dc320d6ef
Sameer Khan <sa74khan@edu.uwaterloo.ca>
2020-09-16 03:03:57 -0400

    Added ability to remove from basket

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,8 +1,11 @@
 import React from "react";
 import "./Checkout.css";
 import Subtotal from "./Subtotal";
+import CheckoutProduct from "./CheckoutProduct";
+import { useStateValue } from "./StateProvider";
 
 function Checkout() {
+  const [{ basket }, dispatch] = useStateValue();
   return (
     <div className="checkout">
       <div className="checkout_left">
@@ -14,6 +17,16 @@ function Checkout() {
 
         <div>
           <h2 className="checkout_title">Your shopping Basket</h2>
+
+          {basket.map(item => (
+            <CheckoutProduct
+              title={item.title}
+              id={item.id}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+            />
+          ))}
         </div>
       </div>
       <div className="checkout_right">
